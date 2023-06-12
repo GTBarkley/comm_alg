8e7ddca721f0f3eb6eca2eb47ac6434ef9328e06
Sameer Savkar <sameersavkar@gmail.com>
2023-06-12 10:34:51 -0700

    created a new file

---
+++ comm_alg/sameer(artinian-rings).lean
@@ -0,0 +1,11 @@
+import Mathlib.RingTheory.Ideal.Basic
+import Mathlib.RingTheory.Noetherian
+import Mathlib.RingTheory.Artinian
+import Mathlib.RingTheory.Ideal.Quotient
+import Mathlib.AlgebraicGeometry.PrimeSpectrum.Basic
+
+lemma quotientRing_is_Artinian (R : Type _) (isArtinianRing R) (Ideal I) : IsArtinianRing R := by sorry
+
+lemma IsPrimeisMaximal (R : Type_) (isArtinianRing R) (Ideal I) : IsPrimeisMaximal R := by sorry
+
+-- Use Stacks project proof since it's broken into lemmas
