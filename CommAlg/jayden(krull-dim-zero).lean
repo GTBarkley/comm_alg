5cb0f77d2f22c5adf957dcacc198be6cf8066751
poincare-duality <jidongw@uoregon.edu>
2023-06-12 20:03:43 -0700

    update

--- CommAlg/jayden(krull-dim-zero).lean
+++ CommAlg/jayden(krull-dim-zero).lean
@@ -4,15 +4,15 @@ import Mathlib.RingTheory.Noetherian
 import Mathlib.Order.KrullDimension
 import Mathlib.RingTheory.Artinian
 import Mathlib.RingTheory.Ideal.Quotient
+import Mathlib.RingTheory.Nilpotent
 import Mathlib.AlgebraicGeometry.PrimeSpectrum.Basic
-
 import Mathlib.AlgebraicGeometry.PrimeSpectrum.Maximal
 import Mathlib.Data.Finite.Defs
-
 import Mathlib.Order.Height
 import Mathlib.RingTheory.DedekindDomain.Basic
 import Mathlib.RingTheory.Localization.AtPrime
 import Mathlib.Order.ConditionallyCompleteLattice.Basic
+import Mathlib.Algebra.Ring.Pi
 
 -- copy from krull.lean; the name of Krull dimension for rings is changed to krullDim' since krullDim already exists in the librrary
 namespace Ideal
@@ -26,20 +26,8 @@ noncomputable def krullDim' (R : Type) [CommRing R] : WithBot ℕ∞ := ⨆ (I :
 
 -- Stacks Lemma 10.60.5: R is Artinian iff R is Noetherian of dimension 0
 lemma dim_zero_Noetherian_iff_Artinian (R : Type _) [CommRing R] : 
-    IsNoetherianRing R ∧ krullDim' R = 0 ↔ IsArtinianRing R := by 
+    IsNoetherianRing R ∧ krullDim' R = 0 ↔ IsArtinianRing R := by sorry
 
-
-variable {R : Type _} [CommRing R]
-
--- Repeats the definition by Monalisa
-noncomputable def length : krullDim (Submodule _ _)
-
-
--- The following is Stacks Lemma 10.60.5
-lemma dim_zero_Noetherian_iff_Artinian (R : Type _) [CommRing R] : 
-    IsNoetherianRing R ∧ krull_dim R = 0 ↔ IsArtinianRing R := by 
-
-  sorry
 
 #check IsNoetherianRing
 
@@ -58,7 +46,25 @@ lemma IsArtinian_iff_finite_length : IsArtinianRing R ↔ ∃ n : ℕ, length R 
 lemma IsArtinian_iff_finite_max_ideal : IsArtinianRing R ↔ Finite (MaximalSpectrum R) := by sorry
 
 -- Stacks Lemma 10.53.4: R Artinian => Jacobson ideal of R is nilpotent
-lemma Jacobson_of_Artinian_is_nilpotent : Is
+lemma Jacobson_of_Artinian_is_nilpotent : IsArtinianRing R → IsNilpotent (Ideal.jacobson (⊤ : Ideal R)) := by sorry
+
+
+-- Stacks Definition 10.32.1: An ideal is locally nilpotent
+-- if every element is nilpotent
+namespace Ideal
+class IsLocallyNilpotent (I : Ideal R) : Prop :=
+    h : ∀ x ∈ I, IsNilpotent x
+
+end Ideal
+
+#check Ideal.IsLocallyNilpotent
+
+-- Stacks Lemma 10.53.5: If R has finitely many maximal ideals and 
+-- locally nilpotent Jacobson radical, then R is the product of its localizations at 
+-- its maximal ideals. Also, all primes are maximal
+
+lemma product_of_localization_at_maximal_ideal : Finite (MaximalSpectrum R)
+     ∧ Ideal.IsLocallyNilpotent (Ideal.jacobson (⊤ : Ideal R)) →  Localization.AtPrime R I
 
 
 
@@ -70,8 +76,6 @@ end something
 
 open something
 
--- The following is Stacks Lemma 10.53.6
-lemma IsArtinian_iff_finite_length : IsArtinianRing R ↔ ∃ n : ℕ, length R R ≤ n := by sorry 
 
 
 
